d42a0b661edb83c9458e916b13ac21dff27bbb7e
Luka Sotra <lukasotra@gmail.com>
2024-01-27 23:22:47 +0100

    added contact page

--- pki_web_app/src/app/(buyer)/buyerContact/page.tsx
+++ pki_web_app/src/app/(buyer)/buyerContact/page.tsx
@@ -2,7 +2,34 @@ import React from 'react'
 
 const BuyerContact = () => {
   return (
-    <div>BuyerContact</div>
+    <div className='mainContainerContact'>
+      <div className='contactContainer'>
+        <div className='firstColumnContact'>
+          <div>
+            Kontakt telefon:
+          </div>
+          <div>
+            Email Adresa:
+          </div>
+          <div>
+            Radno vreme
+          </div>
+        </div>
+        
+        <div className='secondColumnContact'>
+          <div>
+            +384691585262
+          </div>
+          <div>
+            slatkizalogaji@gmail.com
+          </div>
+          <div>
+            Ponedeljak - Petak 9:00 - 18:00 <br/>
+            Subota - Nedelja: Zatvoreno
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
